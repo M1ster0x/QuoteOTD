fa1373b7bf1631fdaf31be73252b07e830f7d117
M1ster0x <43374224+M1ster0x@users.noreply.github.com>
2018-12-11 18:32:45 +0100

    Renommage, Mise à jour du script python
    Ajout de quelques commentaires et petits changements de syntaxe.

--- QuoteOfTheDay.py
+++ QuoteOfTheDay.py
@@ -8,14 +8,15 @@ server.bind(("0.0.0.0", 17))
 server.listen(5)
 
 print("Serveur à l'écoute sur le port 17")
-
 #On écoute sans cesse
 while True:
     sock, addr = server.accept()
     print("Connexion ouverte par {} du port {}".format(sock.getpeername()[0],sock.getpeername()[1]))
+    #On va chercher la quote à renvoyer par l'api
     r = requests.get("https://api.chucknorris.io/jokes/random")
-    quote=r.json()
-    sock.send(bytes(quote["value"] + "\n", "UTF-8"))
+    quote=r.json()["value"] + "\n"
+    #On envoie la quote
+    sock.send(bytes(quote, "UTF-8"))
     print("Réponse envoyée")
     sock.close()
     print("Connexion fermée")
